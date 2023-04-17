c40c8fc9068df2a3f3e54fe7e4858691ab2984d6
Shadito <shadito@live.com.mx>
2023-04-17 16:59:08 -0600

    Change some styles and styled-component with emotion styled

--- src/styled-components/layout.styled.component.ts
+++ src/styled-components/layout.styled.component.ts
@@ -1,7 +1,9 @@
-import styled from 'styled-components'
+import styled from '@emotion/styled'
 
 const LayoutContainer = styled.div`
   width: 80vw;
+  text-align: center;
+  align-self: center;
   @media only screen and (min-width: 1176px) {
     padding: 12px;
   }
